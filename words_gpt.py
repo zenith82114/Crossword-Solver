ac514395c600cac2ac32258476a94ab2377e1a9e
zenith82114 <zenith82114@gmail.com>
2023-12-14 13:17:12 +0900

    Rename

--- words_gpt.py
+++ words_gpt.py
@@ -13,19 +13,19 @@ if __name__ == '__main__':
     print('Sending requests...')
     for clue, clue_dict in grid.items():
         print(f'\tGuessing: {clue}')
-        clue_length = clue_dict['length']
+        ans_length = clue_dict['length']
 
         # v0
         # content = f"""
         #     A crossword clue says: {clue}.
-        #     The answer must be {clue_length} letters long.
+        #     The answer must be {ans_length} letters long.
         #     Output ten most likely answers.
         # """
 
         # v1
         content = f"""
             A crossword clue says: {clue}.
-            The answer must be {clue_length} letters long.
+            The answer must be {ans_length} letters long.
             Output ten most likely answers.
             Do not output anything other than the answers.
             Make sure that the answers are of the same part-of-speech as the clue.
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             max_tokens=50
         )
         output = response.choices[0].message.content
-        guesses = [s.lower() for s in output.split() if s.isalpha() and len(s) == clue_length]
+        guesses = [s.lower() for s in output.split() if s.isalpha() and len(s) == ans_length]
         solution[clue] = list(set(guesses))
 
     print('Storing solution...')
